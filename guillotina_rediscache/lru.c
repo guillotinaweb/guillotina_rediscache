790f0ec67c7e8111ca09ed3b3c23e1a0603af116
Jordi Collell <jordic@gmail.com>
2018-06-09 08:22:07 +0200

    added a benchmark
    This patched original version is faster than the original,
    and also allows us to store more items, the trade off,
    is that you need to set the (aproximated) size of the
    keys (That fits on our use case)
    
    Anyway if we have concerns about, the way we are cleaning,
    the dict.. (To not block too much time), we can add somehting
    like this:
    
    https://github.com/antirez/redis/blob/unstable/src/dict.c#L240
    
    That limits the cleanup for a number of milliseconds, and
    also we can start doing this within a threshold. Anyway, after
    running the benchamrks, doesn't feel necessary for me.

--- guillotina_rediscache/lru.c
+++ guillotina_rediscache/lru.c
@@ -297,13 +297,6 @@ LRU_get(LRU *self, PyObject *args)
 }
 
 
-long long timeInMilliseconds(void) {
-   struct timeval tv;
-
-   gettimeofday(&tv,NULL);
-   return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
-}
-
 static int
 lru_ass_sub(LRU *self, PyObject *key, PyObject *value, Py_ssize_t memory)
 {
