30314ae58d11e19e45154c9eb3d8e0feebe5c53d
jordic <jordic@gmail.com>
2018-06-15 15:13:00 +0200

    some tests

--- guillotina_rediscache/tests/test_lru.py
+++ guillotina_rediscache/tests/test_lru.py
@@ -20,12 +20,25 @@ def test_lrusized_acts_like_a_dict():
 
 
 def test_clean_till_it_has_enought_space():
-    m = LRU(100)
-    for k in range(100):
+    m = LRU(19)
+    for k in range(20):
         m.set(k, k, 1)
-    
 
+    m.set('a', 1, 1)
+    assert 1 not in m.keys()
+    r = m[2]
+    assert r == 2
+    m.set('b', 1, 1)
+    assert 2 in m.keys()
+    assert 3 not in m.keys()
+    m.set('b', 1, 10)
+    assert len(m.keys()) is 10
+    assert 2 in m.keys()
 
+    assert m.get_memory() is 19
+    del m['b']
+    assert m.get_memory() is 9
+    assert len(m.keys()) is 9
 
-
-
+    # we should cleanup 12 keys
+    assert m.get_stats() == (1, 0, 12)
